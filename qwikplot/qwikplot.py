8d8e3c0cb63eef158f958e5d6aff156d732b1c9a
Evan <ecurtin@illinois.edu>
2017-05-01 21:24:35 -0500

    Added set_style

--- qwikplot/qwikplot.py
+++ qwikplot/qwikplot.py
@@ -1,1 +1,23 @@
 # -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+from matplotlib import rcParams as rc
+import seaborn as sns
+
+set_style_valid_args = {'default'}
+def set_style(style='default', save_dir='.'):
+    if style not in set_style_valid_args:
+        raise ValueError('set_style: style must be one of %r' 
+			 % set_style_valid_args)
+    if style == 'default':
+        sns.set_context('paper')
+        sns.set_style('white')
+        sns.set_palette('colorblind')
+
+        rc['text.usetex'] = True
+        rc['font.size'] = 30
+        rc['figure.figsize'] = (5, 5)
+        rc['savefig.format'] = 'pdf'
+        rc['savefig.transparent'] = True
+        rc['savefig.bbox'] = 'tight'
+        rc['savefig.directory'] = os.chdir(save_dir)
--- tests/test_qwikplot.py
+++ tests/test_qwikplot.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """
 test_qwikplot
@@ -9,22 +7,13 @@ Tests for `qwikplot` module.
 """
 
 import pytest
-
-
+import sys
+sys.path.append('../')
 from qwikplot import qwikplot
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_set_style():
+    qwikplot.set_style()
+    qwikplot.set_style('default')
+    qwikplot.set_style('default', save_dir='..')
+    
